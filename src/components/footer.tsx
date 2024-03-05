622824db7bc9d73c9369170409edb7cd67fef985
Jack <26012107+JackTheFlap@users.noreply.github.com>
2024-03-05 19:12:15 +0000

    Minor css changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,8 @@ import { Link } from "@tanstack/react-router"
 
 const Footer = () => {
     return (<footer className="flex flex-col items-center justify-center mt-auto mb-4">
-        <div className="flex flex-row items-center justify-center">
-            <h6 className="mr-1">Made with ❤️ by <a href="https://jacktheflap.com" className="underline">JackTheFlap</a> - </h6>
-            <Link to="/privacy-policy" className="mr-2 underline">Privacy Policy</Link>
-        </div>
+        <h6 className="mr-1">Made with ❤️ by <a href="https://jacktheflap.com" className="underline">JackTheFlap</a></h6>
+        <Link to="/privacy-policy" className="mr-2 underline">Privacy Policy</Link>
         <p>{new Date(Date.now()).getFullYear()}</p>
     </footer>)
 }
