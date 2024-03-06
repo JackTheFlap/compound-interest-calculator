39eff5fb855c2d1a2b220fa180bc7e03b04d0d81
Jack <26012107+JackTheFlap@users.noreply.github.com>
2024-03-06 20:38:19 +0000

    Added robots.txt Updated aria-label for nav elements

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -8,18 +8,18 @@ const NavMenu = () => {
     return (
         <nav className="flex flex-col items-center mt-4 mb-4 sm:flex-row sm:mb-4 animate-smooth-fade-in">
             <a href="https://jacktheflap.com">
-                <Avatar className="w-10 h-10 mr-4 sm:h-14 sm:w-14">
-                    <AvatarImage src="/me.jpg"></AvatarImage>
+                <Avatar className="w-10 h-10 mb-4 mr-0 sm:mb-0 sm:mr-4 sm:h-14 sm:w-14">
+                    <AvatarImage alt="JackTheFlap" src="/me.jpg"></AvatarImage>
                     <AvatarFallback>JackTheFlap</AvatarFallback>
                 </Avatar>
             </a>
-            <Link to="/">
+            <Link to="/" className="mb-4 sm:mb-0">
                 <h1 className="text-center sm:text-left">Compound Interest Calculator</h1>
             </Link>
             <div className="flex flex-row items-center sm:ml-auto">
                 <ModeToggle />
                 <a className="ml-2" href="https://github.com/jacktheflap/compound-interest-calculator">
-                    <Button variant={"outline"} className="p-2"><Github /></Button>
+                    <Button aria-label="Github Repository" variant={"outline"} className="p-2"><Github /></Button>
                 </a>
             </div>
 
