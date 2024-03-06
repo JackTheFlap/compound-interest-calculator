7d61f1e908f0ca2c238fd8842b66964ed3c12904
Jack <26012107+JackTheFlap@users.noreply.github.com>
2024-03-06 20:53:47 +0000

    Added fade in for 404 page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,14 +4,14 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
     component: () => (
-        <main className='container flex flex-col min-h-screen'>
+        <main className='container flex flex-col min-h-screen animate-smooth-fade-in'>
             <NavMenu />
             <Outlet />
             <Footer />
         </main>
     ),
     notFoundComponent: () => {
-        return (<main className='container flex flex-col min-h-screen'>
+        return (<main className='container flex flex-col min-h-screen animate-smooth-fade-in'>
             <NavMenu />
             <section className='container flex flex-col items-center justify-center mt-8'>
                 <p className='text-6xl font-black'>Page Not Found</p>
