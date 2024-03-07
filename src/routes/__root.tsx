844553d73ea9741fa2d1ab3ce164fdc474034cd2
Jack <26012107+JackTheFlap@users.noreply.github.com>
2024-03-07 09:54:47 +0000

    Added margin for mobile layouts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,9 +13,9 @@ export const Route = createRootRoute({
     notFoundComponent: () => {
         return (<main className='container flex flex-col min-h-screen animate-smooth-fade-in'>
             <NavMenu />
-            <section className='container flex flex-col items-center justify-center mt-8'>
-                <p className='text-6xl font-black'>Page Not Found</p>
-                <Link to="/" className='text-3xl underline'>Go Home?</Link>
+            <section className='container flex flex-col items-center justify-center mt-8 mb-4'>
+                <p className='text-6xl font-black text-center'>Page Not Found</p>
+                <Link to="/" className='mt-4 text-3xl text-center underline sm:mt-4'>Go Home?</Link>
             </section>
             <Footer />
         </main>);
